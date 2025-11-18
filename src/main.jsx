2196cf13b8be8b44256b6dec5fc38bdb83c3e98d
Santiago Camilo Rey Benavides <screyb@javeriana.edu.co>
2025-11-17 19:44:27 -0500

    add: analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./routes/AppRoutes.jsx";
 import "./index.css";
-
+import { Analytics } from "@vercel/analytics/react"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppRoutes />
+    <Analytics />
   </React.StrictMode>
+  
 );
